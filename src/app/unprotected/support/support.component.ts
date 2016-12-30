1784a97f163c89d1516f303c4e87b9c4849673ff
wazaniq <vechesumstudent@gmail.com>
2016-12-30 02:55:37 +0200

    Home page welcome card, and other cosmetic changes

--- src/app/unprotected/support/support.component.ts
+++ src/app/unprotected/support/support.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {TrollboxComponent} from '../../shared/trollbox/trollbox.component';
+
 @Component({
   selector: 'app-support',
   templateUrl: './support.component.html',
@@ -8,6 +10,15 @@ import { Component, OnInit } from '@angular/core';
 export class SupportComponent implements OnInit {
 
   posts=[
+    {title:'Technical maintenance (плановые технические работы)', by:'Jango A', createdOn:'30 September 2016 04:18 PM', body:'Technical maintenance in data center', avatar:'http://www.freeiconspng.com/uploads/profile-icon-9.png'},
+    {title:'Technical maintenance (плановые технические работы)', by:'Jango A', createdOn:'30 September 2016 04:18 PM', body:'Technical maintenance in data center', avatar:'http://www.freeiconspng.com/uploads/profile-icon-9.png'},
+    {title:'Technical maintenance (плановые технические работы)', by:'Jango A', createdOn:'30 September 2016 04:18 PM', body:'Technical maintenance in data center', avatar:'http://www.freeiconspng.com/uploads/profile-icon-9.png'},
+    {title:'Technical maintenance (плановые технические работы)', by:'Jango A', createdOn:'30 September 2016 04:18 PM', body:'Technical maintenance in data center', avatar:'http://www.freeiconspng.com/uploads/profile-icon-9.png'},
+    {title:'Technical maintenance (плановые технические работы)', by:'Jango A', createdOn:'30 September 2016 04:18 PM', body:'Technical maintenance in data center', avatar:'http://www.freeiconspng.com/uploads/profile-icon-9.png'},
+    {title:'Technical maintenance (плановые технические работы)', by:'Jango A', createdOn:'30 September 2016 04:18 PM', body:'Technical maintenance in data center', avatar:'http://www.freeiconspng.com/uploads/profile-icon-9.png'},
+    {title:'Technical maintenance (плановые технические работы)', by:'Jango A', createdOn:'30 September 2016 04:18 PM', body:'Technical maintenance in data center', avatar:'http://www.freeiconspng.com/uploads/profile-icon-9.png'},
+    {title:'Technical maintenance (плановые технические работы)', by:'Jango A', createdOn:'30 September 2016 04:18 PM', body:'Technical maintenance in data center', avatar:'http://www.freeiconspng.com/uploads/profile-icon-9.png'},
+    {title:'Technical maintenance (плановые технические работы)', by:'Jango A', createdOn:'30 September 2016 04:18 PM', body:'Technical maintenance in data center', avatar:'http://www.freeiconspng.com/uploads/profile-icon-9.png'},
     {title:'Technical maintenance (плановые технические работы)', by:'Jango A', createdOn:'30 September 2016 04:18 PM', body:'Technical maintenance in data center', avatar:'http://www.freeiconspng.com/uploads/profile-icon-9.png'},
     {title:'Technical maintenance (плановые технические работы)', by:'Jango A', createdOn:'30 September 2016 04:18 PM', body:'Technical maintenance in data center', avatar:'http://www.freeiconspng.com/uploads/profile-icon-9.png'}
   ];
