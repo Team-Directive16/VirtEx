6a0b1ba8c58676da147f4b3631cba4ca698de13e
madmarks <bratmiro@gmail.com>
2016-12-30 10:53:21 +0200

    add AuthService and AuthGuard to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -10,9 +11,8 @@ import { NewsComponent } from './unprotected/news/news.component';
 import { NewsDetailsComponent } from './unprotected/news/details/news-details.component';
 import { TermsComponent } from './unprotected/terms/terms.component';
 import { FaqComponent } from './unprotected/faq/faq.component';
-import {AskComponent} from './unprotected/faq/ask/ask.component';
+import { AskComponent } from './unprotected/faq/ask/ask.component';
 import { SupportComponent } from './unprotected/support/support.component';
-import { routing } from './app.routing';
 import { ExchangeComponent } from './unprotected/exchange/exchange.component';
 import { LoginComponent } from './unprotected/login/login.component';
 import { SignupComponent } from './unprotected/signup/signup.component';
@@ -20,7 +20,11 @@ import { TrollboxComponent } from './shared/trollbox/trollbox.component';
 import { BuyBoxComponent } from './shared/buy-box/buy-box.component';
 import { SellBoxComponent } from './shared/sell-box/sell-box.component';
 import { SellOrdersComponent } from './unprotected/exchange/sell-orders/sell-orders.component';
-import { BuyOrdersComponent } from './unprotected/exchange/buy-orders/buy-orders.component'
+import { BuyOrdersComponent } from './unprotected/exchange/buy-orders/buy-orders.component';
+import { AuthGuard } from "./shared/auth.guard";
+import { AuthService } from './shared/auth.service';
+import { ProfileComponent } from './protected/profile/profile.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
@@ -40,15 +44,17 @@ import { BuyOrdersComponent } from './unprotected/exchange/buy-orders/buy-orders
     BuyBoxComponent,
     SellBoxComponent,
     SellOrdersComponent,
-    BuyOrdersComponent
+    BuyOrdersComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    ReactiveFormsModule,
     routing
   ],
-  providers: [],
+  providers: [AuthGuard, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
