f5fa099602dc6125e3fb18f101641daab9710259
madmarks <bratmiro@gmail.com>
2016-12-30 10:55:46 +0200

    introduce signup form and related functionality to register user

--- src/app/unprotected/signup/signup.component.ts
+++ src/app/unprotected/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from "@angular/forms";
+
+import { AuthService } from "../../shared/auth.service";
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  myForm: FormGroup;
+  error = false;
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private authService: AuthService) { }
 
-  ngOnInit() {
+  onSignup() {
+    this.authService.signupUser(this.myForm.value);
   }
 
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      email: ['', Validators.compose([
+        Validators.required,
+        this.isEmail
+      ])],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.compose([
+        Validators.required,
+        this.isEqualPassword.bind(this)
+      ])]
+    });
+  }
+
+  isEmail(control: FormControl): { [s: string]: boolean } {
+    if (!control.value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
+      return { noEmail: true };
+    }
+  }
+
+  isEqualPassword(control: FormControl): { [s: string]: boolean } {
+    if (!this.myForm) {
+      return { passwordsNotMatch: true };
+    }
+
+    if (control.value !== this.myForm.controls['password'].value) {
+      return { passwordsNotMatch: true };
+    }
+  }
 }
