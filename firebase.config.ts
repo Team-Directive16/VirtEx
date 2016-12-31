24390ef9bdf1efd7a91b3977671765334c5f7282
wazaniq <vechesumstudent@gmail.com>
2016-12-31 02:33:32 +0200

    Forgot configuration for AngularFire
    Ups.

---
+++ firebase.config.ts
@@ -0,0 +1,7 @@
+export const config = {
+    apiKey: "AIzaSyAra35ie7SFEZpExoQmYAuFAY-gXDfwngU",
+    authDomain: "virtex-d95cd.firebaseapp.com",
+    databaseURL: "https://virtex-d95cd.firebaseio.com",
+    storageBucket: "virtex-d95cd.appspot.com",
+    messagingSenderId: "521546189377"
+  };
