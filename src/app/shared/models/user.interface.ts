ff63ba56d53ea75142caa3364663e7fa8793e616
madmarks <bratmiro@gmail.com>
2017-01-01 04:58:08 +0200

    add more properties to User interface

--- src/app/shared/models/user.interface.ts
+++ src/app/shared/models/user.interface.ts
@@ -2,4 +2,9 @@ export interface User {
     email: string;
     password: string;
     confirmPassword?: string;
+    firstName?: string;
+    lastName?: string;
+    birthDate?: Date;
+    gender?: string;
+    photoURL?: string;
 }
