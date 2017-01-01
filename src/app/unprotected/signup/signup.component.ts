eafae9213c22f10684100f52e8cfdfdfb562cd19
madmarks <bratmiro@gmail.com>
2017-01-01 09:57:06 +0200

    create new user account and add profile in firebase DB for each created user

--- src/app/unprotected/signup/signup.component.ts
+++ src/app/unprotected/signup/signup.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from "@angular/forms";
-//import { SimpleNotificationsComponent } from 'angular2-notifications';
 import { NotificationsService } from 'angular2-notifications';
+import { Title } from "@angular/platform-browser";
 
-
-import { AuthService } from "../../shared/auth.service";
+import { AuthService } from "../../shared/services/index";
 
 @Component({
   selector: 'app-signup',
@@ -13,47 +12,53 @@ import { AuthService } from "../../shared/auth.service";
 })
 export class SignupComponent implements OnInit {
   myForm: FormGroup;
-  error = false;
-  errorMessage = '';
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private notificationService: NotificationsService) { }
+  constructor(
+    private _fb: FormBuilder,
+    private _title: Title,
+    private _authService: AuthService, 
+    private _notificationsService: NotificationsService) { }
 
-  onSignup() {
-    if(this.myForm.invalid){
-      this.notificationService.error("Something went wrong", "Incorrect email or password");
+  onSignUp() {
+    if (this.myForm.invalid) {
+      this._notificationsService.error('Incorrect email or password', 'Please, try again');
+      return;
     }
-    this.authService.signupUser(this.myForm.value);
-    this.notificationService.success("You registered successfuly", "Welcome!");
 
+    this._authService
+      .signUp(this.myForm.value)
+      .then(success => {
+        if (success) {
+          this._notificationsService.success('Welcome!', 'User: ' + this._authService.authenticatedUser.email + ' registered successfully')
+        } else {
+          this._notificationsService.error('Error', 'Duplicate user or connection problems')
+        }
+      })
   }
 
-  ngOnInit() {
-    this.myForm = this.fb.group({
-      email: ['', Validators.compose([
-        Validators.required,
-        this.isEmail
-      ])],
+  ngOnInit(): any {
+    this.myForm = this._fb.group({
+      email: ['', Validators.compose([Validators.required, this.isEmail])],
       password: ['', Validators.required],
-      confirmPassword: ['', Validators.compose([
-        Validators.required,
-        this.isEqualPassword.bind(this)
-      ])]
+      confirmPassword: ['', Validators.compose([Validators.required, this.isEqualPassword.bind(this)])]
     });
+
+    this._title.setTitle('Sign up');  //page title
   }
 
   isEmail(control: FormControl): { [s: string]: boolean } {
     if (!control.value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
-      return { noEmail: true };
+      return { noEmail: true }
     }
   }
 
   isEqualPassword(control: FormControl): { [s: string]: boolean } {
     if (!this.myForm) {
-      return { passwordsNotMatch: true };
+      return { passwordsNotMatch: true }
     }
 
     if (control.value !== this.myForm.controls['password'].value) {
-      return { passwordsNotMatch: true };
+      return { passwordsNotMatch: true }
     }
   }
 }
