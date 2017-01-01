ff704b239550e51bdff4b52b417f4109464f8984
madmarks <bratmiro@gmail.com>
2017-01-01 05:37:34 +0200

    use header component as a single place to show notifications

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFire, FirebaseAuth } from 'angularfire2';
+import { SimpleNotificationsComponent, NotificationsService } from 'angular2-notifications';
 
-import { AuthService } from "../auth.service";
+import { AuthService } from "../services/auth.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor(private authService: AuthService) { }
-
-  ngOnInit() {
-    this.isAuth();
+export class HeaderComponent {
+  private options = {
+    timeOut: 4000,
+    position: ["top", "left"],
+    showProgressBar: false,
+    preventDuplicates: true
   }
 
-  isAuth() {
-    return this.authService.isAuthenticated();
-  }
+  constructor(
+    private _af: AngularFire,
+    private _authService: AuthService,
+    private _notificationsService: NotificationsService) { }
 
   onLogout() {
-    this.authService.logout();
+    this._authService.logout();
+    this._notificationsService.success('Success', 'User: ' + this._authService.authenticatedUser.email + ' successfully signed-out')
   }
 }
