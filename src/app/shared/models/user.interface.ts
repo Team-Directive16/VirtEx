e644d08d6ea0e5c6b491ee20d886a4fe6889a14b
madmarks <bratmiro@gmail.com>
2017-01-01 10:03:26 +0200

    add username property in User interface

--- src/app/shared/models/user.interface.ts
+++ src/app/shared/models/user.interface.ts
@@ -1,10 +1,15 @@
 export interface User {
+    username?: string;
     email: string;
     password: string;
     confirmPassword?: string;
     firstName?: string;
     lastName?: string;
-    birthDate?: Date;
+
+    // var x = new Date(JSON.parse(JSON.stringify(new Date())));
+    // http://stackoverflow.com/questions/11491938/issues-with-date-when-using-json-stringify-and-json-parse
+    birthDate?: string;
+
     gender?: string;
     photoURL?: string;
 }
